17bbd95da0fe6e0616bbdf3b3c2c573f68b153b0
gduartesilva519-coder <gduartesilva519@gmail.com>
2026-09-14 15:13:49 -0300

    first commit

---
+++ gerar-env.ps1
@@ -0,0 +1,1 @@
+# -Forcar para sobrescrever
---
+++ initdb/10-app-db.sql
@@ -0,0 +1,5 @@
+
+
+CREATE DATABASE app_corporativo;
+CREATE USER app_user WITH PASSWORD '12345';
+GRANT ALL PRIVILEGES ON DATABASE app_corporativo TO app_user;
