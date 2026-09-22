5d384608434b52bd5a8109c29882443d0ca8c724
gduartesilva519-coder <gduartesilva519@gmail.com>
2026-09-22 13:16:43 -0300

    Atuliza códigos do projeto

--- dags/executar_job_produto.py
+++ dags/executar_job_produto.py
@@ -1,19 +1,51 @@
 from airflow.sdk import DAG
 from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 from datetime import datetime
 
 
 def executar_job(**context):
-    conf = context["dag_run"].conf
 
-    job_id = conf["job_id"]
-    produto = conf["produto"]
+    conf = context["dag_run"].conf or {}
 
-    print("Executando Job no produto...")
+    print("==========================")
+    print("DADOS RECEBIDOS:")
+    print("==========================")
+
+    print("CONF:", conf)
+
+    job_id = conf.get("job_id")
+    produto = conf.get("produto")
+
     print("Job ID:", job_id)
     print("Produto:", produto)
-    print("CONF RECEBIDO:", conf)
+
+    if not job_id:
+        print("Nenhum Job recebido.")
+        return
+
+    postgres = PostgresHook(postgres_conn_id="postgres_jobs")
+
+    conexao = postgres.get_conn()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        UPDATE public.jobs_produto
+        SET status = 'CONCLUIDO'
+        WHERE id = %s;
+    """, (job_id,))
+
+    conexao.commit()
+
+    print("=======================")
+    print("JOB CONCLUIDO:")
+    print("=======================")
+    print("Job_ID:", job_id)
+    print("Produto:", produto)
+
+    cursor.close()
+    conexao.close()
 
 with DAG(
     dag_id="executar_job_produto",
@@ -26,4 +58,3 @@ with DAG(
         task_id="executar_job",
         python_callable=executar_job
     )
-
--- dags/verificar_jobs.py
+++ dags/verificar_jobs.py
@@ -1,46 +1,86 @@
 from airflow.sdk import DAG
-from airflow.providers.standard.operators.python import PythonOperator
-from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.standard.operators.python import PythonOperator, BranchPythonOperator
 from airflow.providers.standard.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 from datetime import datetime
 
 
 def verificar_jobs():
+
+    
     postgres = PostgresHook(postgres_conn_id="postgres_jobs")
 
     conexao = postgres.get_conn()
-    cursor = conexao.cursor()
+    cursor =  conexao.cursor()
 
     cursor.execute("""
-        SELECT id, produto, status, data_hora_execucao
-        FROM jobs_produto
+        SELECT id, status, produto, data_hora_execucao
+        FROM public.jobs_produto
         WHERE status = 'AGENDADO'
-          AND data_hora_execucao <= NOW()
-        ORDER BY id;
+            AND data_hora_execucao <= NOW()
+        ORDER BY id
+        LIMIT 1;
     """)
 
     jobs = cursor.fetchall()
 
+    print("=========================")
+    print("JOBS ENCONTRADOS:")
+    print("=========================")
+
     for job in jobs:
         print(job)
+
+    if jobs:
+        job_id = jobs[0][0]
+
+        cursor.execute("""
+            UPDATE public.jobs_produto
+            SET status = 'PROCESSANDO'
+            WHERE id = %s;
+        """, (job_id,))
+
+        conexao.commit()
+
+        print("=========================")
+        print("JOB MARCADO COMO PROCESSANDO:")
+        print("=========================")
+        print("Job ID:", job_id)
 
     cursor.close()
     conexao.close()
 
-    return [
-        {
-            "job_id": job[0],
-            "produto": job[1]
-        }
-        for job in jobs
-    ]
+    dados_jobs = []
 
+    for job in jobs:
+        dados_jobs.append({
+            "job_id": job [0],
+            "produto": job [2]
+        })
+
+    print("=========================")
+    print("DADOS PARA EXECUÇÃO:")
+    print("=========================")
+    print(dados_jobs)
+
+    return dados_jobs[0] if dados_jobs else {}
+
+def decidir_proximo_passo(**context):
+
+    dados = context["ti"].xcom_pull(
+        task_ids="verificar_jobs_postgres"
+    )
+
+    if dados:
+        return "disparar_job"
+
+    return "fim"
 
 with DAG(
     dag_id="verificar_jobs",
     start_date=datetime(2026, 1, 1),
-    schedule=None,
+    schedule="*/1 * * * *",
     catchup=False
 ) as dag:
 
@@ -49,11 +89,23 @@ with DAG(
         python_callable=verificar_jobs
     )
 
-    disparar_job = TriggerDagRunOperator.partial(
+    decidir = BranchPythonOperator(
+        task_id="decidir",
+        python_callable=decidir_proximo_passo
+    )
+
+    disparar_job = TriggerDagRunOperator(
         task_id="disparar_job",
         trigger_dag_id="executar_job_produto",
-    ).expand(
         conf=tarefa_verificar.output
     )
 
-    tarefa_verificar >> disparar_job
+    fim = PythonOperator(
+        task_id="fim",
+        python_callable=lambda: print("Nenhum Job para executar.")
+    )
+
+    tarefa_verificar >> decidir
+
+    decidir >> disparar_job
+    decidir >> fim
